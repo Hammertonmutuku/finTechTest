2292b1a41ae5123d6e15f61093f8447f5ce9b7d8
Hammerton Muinde (Techno Brain Global FZ LLC) <v-hmuinde@microsoft.com>
2023-11-24 10:53:39 +0300

    wrote the function

--- TechTest/Program.cs
+++ TechTest/Program.cs
@@ -4,7 +4,35 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string apiUrl = "https://example.com/api/sampleEndpoint";
+
+            try
+            {
+                // Send GET request and validate response
+                string responseData = await GetApiResponse(apiUrl);
+
+                // Process the response data as needed
+                Console.WriteLine($"Response from the API: {responseData}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error: {ex.Message}");
+            }
+        }
+        static async Task<string> GetApiResponse(string apiUrl)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                // Send GET request
+                HttpResponseMessage response = await client.GetAsync(apiUrl);
+
+                // Check if the request was successful
+                response.EnsureSuccessStatusCode();
+
+                // Read and return the response content as a string (assuming JSON data)
+                return await response.Content.ReadAsStringAsync();
+            }
         }
     }
+   
 }
